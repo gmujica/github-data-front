884fc9ea8a05e4167486d8aba029efd2520b5110
gmujica <gregorymujica1993@gmail.com>
2022-04-20 17:12:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [user, setUser] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getUserData = () => {
+    axios.get('http://localhost:5000/api/github/userinfo/gmujica')
+    .then(res => {
+      //setIsLoading(false)
+      setUser(res.data);
+      
+      console.log(user);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  const getRepositoryData = () => {
+    axios.get('http://localhost:5000/api/github/repoinfo/gmujica/github-node-api')
+    .then(res => {
+      console.log(res.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  const getCommitsData = () => {
+    axios.get('http://localhost:5000/api/github/commitinfo/gmujica/github-node-api')
+    .then(res => {
+      console.log(res.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={getUserData}>
+          Get User Data
+        </button>
+        <button onClick={getRepositoryData}>
+          Get Repository Data
+        </button>
+        <button onClick={getCommitsData}>
+          Get Commits Data
+        </button>
       </header>
+      {isLoading && <p>Loading Items...</p>}
     </div>
   );
 }
