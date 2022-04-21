8d0c5b525d77124a3211e91dd94d7e4743b64c01
gmujica <gregorymujica1993@gmail.com>
2022-04-21 18:51:57 -0500

    base done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 export default function SimpleContainer() {
   const [commit, setCommit] = useState([]);
@@ -20,12 +26,32 @@ export default function SimpleContainer() {
 
 
   return (
-      <div>
-        {commit.map((co, i) => {
-          return 
-            <div key={i}>{co.commit.message}</div>
-        })}
-      </div>
-    
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Message</TableCell>
+            <TableCell align="right">Author</TableCell>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">Email</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {commit.map((co, i) => (
+            <TableRow
+            key={i}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {co.commit.message}
+              </TableCell>
+              <TableCell align="right">{co.commit.author.name}</TableCell>
+              <TableCell align="right">{co.commit.author.date}</TableCell>
+              <TableCell align="right">{co.commit.author.email}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
-}
+};
