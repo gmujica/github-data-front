ccd7a92d9725a937aad558e9f0e8aca13c1e7307
gmujica <gregorymujica1993@gmail.com>
2022-04-25 11:24:33 -0500

    new card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import User from './components/User'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <User />
       <Main />
     </div>
   );
